08a7f2b47d46b80d4627f9ab0c3591e86e14aa67
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-08-06 20:44:35 +0200

    Corner values added dinamically

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const radiusValue = document.querySelector('#radius-value')
 const square = document.querySelector('#square')
-const radiusSlider = document.querySelector('#radius-value')
 
-console.log(radiusSlider)
+const radiusValueTL = document.querySelector('#radius-value-tl')
+const radiusValueTR = document.querySelector('#radius-value-tr')
+const radiusValueBL = document.querySelector('#radius-value-bl')
+const radiusValueBR = document.querySelector('#radius-value-br')
+
+
 
 radiusValue.addEventListener('change', () => {
     const radiusValueVal = radiusValue.value
@@ -12,3 +16,34 @@ radiusValue.addEventListener('change', () => {
     console.log(radiusValueVal)
 })
 
+radiusValueTL.addEventListener('change', () => {
+    const radiusValueVal = radiusValueTL.value
+
+    square.style.borderTopLeftRadius = `${radiusValueVal}px`
+    radiusValueTL.style.borderTopLeftRadius = `${radiusValueVal}px`
+
+})
+
+radiusValueTR.addEventListener('change', () => {
+    const radiusValueVal = radiusValueTR.value
+
+    square.style.borderTopRightRadius = `${radiusValueVal}px`
+    radiusValueTR.style.borderTopRightRadius = `${radiusValueVal}px`
+
+})
+
+radiusValueBL.addEventListener('change', () => {
+    const radiusValueVal = radiusValueBL.value
+
+    square.style.borderBottomLeftRadius = `${radiusValueVal}px`
+    radiusValueBL.style.borderBottomLeftRadius = `${radiusValueVal}px`
+
+})
+
+radiusValueBR.addEventListener('change', () => {
+    const radiusValueVal = radiusValueBR.value
+
+    square.style.borderBottomRightRadius = `${radiusValueVal}px`
+    radiusValueBR.style.borderBottomRightRadius = `${radiusValueVal}px`
+
+})
